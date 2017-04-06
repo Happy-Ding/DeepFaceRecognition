552e5b50fad6aba0c92acc07546a4595d1486f1d
bb2103 <bb2103@qq.com>
2017-04-06 19:53:07 +0800

    Tool Dataset List

---
+++ Tools/DatasetList/README.md
@@ -0,0 +1,3 @@
+# Dataset List Helper
+Get Dataset List With Clean
+Models used to clean dataset prepared is Center Loss.
--- Tools/DatasetList/get_clean_dataset_list.py
+++ Tools/DatasetList/get_clean_dataset_list.py
@@ -3,27 +3,27 @@ import os
 import caffe
 
 # Configuration
-network_proto_path = ''
-network_model_path = ''
-data_mean = []
-dataset_dir = 'Z:\\CASIA-WebFace-Clean-align'
-visual = True
+network_proto_path = 'Models/deploy.prototxt'
+network_model_path = 'Models/face_model.caffemodel'
+data_mean = np.array([127.5, 127.5, 127.5])
+dataset_dir = 'Z:\\CASIA-WebFace-Clean-align' #msceleb-align
+visual = False
 
 # Init Caffe Related
 caffe.set_mode_gpu()
 net = caffe.Net(network_proto_path, network_model_path, caffe.TEST)
 transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
-if data_mean != None:
+if data_mean is not None:
   transformer.set_mean('data', data_mean)
 transformer.set_raw_scale('data', 255) # 256
 transformer.set_transpose('data', (2,0,1))
 transformer.set_channel_swap('data', (2,1,0))
 batch_size = net.blobs['data'].data.shape[0]
-print "Batch Size: ", batch_size
+print("Batch Size: "+str(batch_size))
 #print "Feature Layer Shape: ", net.blobs[layer_name].data.shape
 
 # Extract Features based on images path list
-def extract_features(image_list, layer_name='fc6'):    
+def extract_features(image_list, layer_name='fc5'):    
     length = len(image_list)
     features = np.empty((length, net.blobs[layer_name].data.shape[1]), dtype='float32')
     img_batch = []
@@ -37,20 +37,31 @@ def extract_features(image_list, layer_name='fc6'):
     return features
 
 if __name__ == "__main__":
-  for folder in os.listdir(dataset_dir):
+  records = []
+  number_total = 0
+  for index, folder in enumerate(os.listdir(dataset_dir)):
     folder_dir = os.path.join(dataset_dir, folder)
-    images_path = [os.path.join(folder_dir, item) for item in os.listdir(folder_dir)]
+    images_path = np.array([os.path.join(folder_dir, item) for item in os.listdir(folder_dir)])
     features = extract_features(images_path)
     feature_center = np.median(features, axis=0)
     diff = np.subtract(np.array([feature_center]), features)
     l2dist = np.int32(np.sum(np.square(diff),1))
+    for item in images_path[l2dist <= 650]:
+      records.append((item, index))
+    number_total += images_path.shape[0]
     # Visual L2 Distance
     if visual:
+      # Python3 SeaBorn Bug FIX: 
+      # conda uninstall statsmodels --yes
+      # conda install -c taugspurger statsmodels=0.8.0
       import seaborn as sns
       import matplotlib.pyplot as plt
-      ax = sns.distplot(l2dist)
+      ax = sns.kdeplot(l2dist, shade=True)
       ax.set_xlabel('L2 Distance')
       ax.set_ylabel('Probability')
-      ax.set_title(folder)
-      ax.grid(True)
+      ax.set_title(folder_dir)
       plt.show()
+  print("Total: "+str(number_total))
+  print("Output: "+str(len(records)))
+  with open("list.txt", 'w') as f:
+    f.write('\n'.join([path+' '+str(label) for path, label in records]))
